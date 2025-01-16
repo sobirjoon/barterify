50da2e220616835453629cefa2b0a260e39ba26e
Sobirjon <sobirjoon@outlook.com>
2025-01-16 20:33:21 +0000

    BAR-001: Updated auth forms and navbar.
    Updated auth forms with better design and layout. Fixed active link issues in navbar.

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-        <div>
-            <slot name="logo" />
-        </div>
-
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
-            <slot />
+        <div class="card bg-base-100 w-full sm:max-w-md shadow-xl overflow-hidden sm:rounded-lg">
+            <figure>
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                    alt="Illustration of account login"
+                    class="w-full object-cover"
+                />
+            </figure>
+            <div class="card-body space-y-4">
+                <h2 class="card-title text-lg font-semibold">{{ $page.component === 'Auth/Register' ? 'Create a new account' : 'Login to your account' }}</h2>
+                    <slot/>
+            </div>
         </div>
     </div>
 </template>
+<script setup lang="ts">
+</script>
--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -14,8 +14,8 @@ const props = defineProps({
 });
 const classes = computed(() => {
     return props.active
-        ? 'bg-primary text-primary-content font-semibold transition-colors duration-200'
-        : 'hover:bg-base-200 hover:text-base-content transition-colors duration-200';
+        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
+        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 });
 </script>
 
